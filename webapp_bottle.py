f4cd5cf4d0960da90f7bc08a7cb33fed581e1095
stutimishra9 <32788677+stutimishra9@users.noreply.github.com>
2020-01-12 22:49:15 -0800

    Add files via upload

--- webapp_bottle.py
+++ webapp_bottle.py
@@ -25,11 +25,28 @@ def verifypage():
     if not(u=='abc' and p=='xyz'):
         return 'LOGIN FAILED'
     else:
-        return 'SUCCESS'
-
-
-
-
-
-
+        #return 'LOGIN SUCCESS'
+        bottle.TEMPLATE_PATH.append(r'C:\pytraining\bin')
+        import sqlite3
+        con=sqlite3.connect('mydb.sqlite3')
+        cur=con.cursor()
+        cur.execute('SELECT * FROM LOGDATA')
+        result=cur.fetchall()
+        return bottle.template('report.tpl',res=result)
+@app.route('/download/<filename>')
+def staticfile(filename):
+    return bottle.static_file(root=r'C:\Python Training\bin',filename=filename)
+@app.route('/empid/<eid:int>')
+def empid(eid):
+    D={'name':'abc','EMP_ID':eid}
+    return D
+@app.route('/nameid/<nid:re:[a-z0-9]+>')
+def nameid(nid):
+    return str(nid)
+@app.route('/logdata')
+def logdata():
+    return bottle.redirect('/login')
+@app.route('/passwords')
+def passwords():
+    return bottle.abort(201,'AccessDenied')
 app.run(host='192.168.3.253',port=8080)
